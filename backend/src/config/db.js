acdaf1f54d700b03b85d53ef93d7bd8771dbdd3c
Areeba Javed <areejaved337@gmail.com>
2026-08-09 18:09:26 +0500

    chnge

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,16 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-async function connectDB(){
-   console.log("connectDB called!")
-    try{
-       await mongoose.connect(process.env.MONGO_URI) 
-       console.log("Database is connected.")
-    }
-    catch(err){
-       console.log("error coming from fetching the db",err)
-    }
-       
+async function connectDB() {
+  console.log("connectDB called!");
+  console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database is connected.");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    throw err;
+  }
 }
-module.exports=connectDB;
+
+module.exports = connectDB;
