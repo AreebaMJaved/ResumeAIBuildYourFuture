fa5966e50ae0f32f01176fb8e4ced579ac17f9f3
Areeba Javed <areejaved337@gmail.com>
2026-08-09 16:31:18 +0500

    ADD vercel SPA setting

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create reusable axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
